4a66f955993731b61a0c9b13d844b25c627b9568
RookieWangZhiWei <RookieWangZhiWei@163.com>
2019-04-03 23:17:42 +0800

    brand nginx

---
+++ leyou-item/leyou-item-interface/src/main/java/com/leyou/item/pojo/Category.java
@@ -0,0 +1,26 @@
+package com.leyou.item.pojo;
+
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by RookieWangZhiWei on 2019/4/3.
+ */
+@Table(name="tb_category")
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Long parentId;
+    private Boolean isParent;
+
+    private Integer sort;
+    // getter和setter略
+    // 注意isParent的get和set方法
+}
---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/mapper/BrandMapper.java
@@ -0,0 +1,10 @@
+package com.leyou.mapper;
+
+import com.leyou.item.pojo.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * Created by RookieWangZhiWei on 2019/4/3.
+ */
+public interface BrandMapper extends Mapper<Brand> {
+}
